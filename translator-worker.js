b574b1309e01250527f2386ba0565c668996918a
Andhs-eff <106329215+Andhs-eff@users.noreply.github.com>
2025-03-14 13:04:48 +0300

    Add files via upload

--- translator-worker.js
+++ translator-worker.js
@@ -1,10 +1,11 @@
 // translator-worker.js
-import { pipeline } from 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.4.0';
+import { pipeline } from 'https://cdn.jsdelivr.net/npm/@huggingface/transformers@3.3.3';
 
 let generator;
 
 async function initializeGenerator() {
-  generator = await pipeline('text-generation', 'onnx-community/gemma-3-1b-it-ONNX', { dtype: 'int8' });
+  //generator = await pipeline('text-generation', 'onnx-community/Qwen2.5-0.5B-Instruct', { dtype: 'q4' });
+  generator = await pipeline('translation', 'Xenova/opus-mt-ru-en');
   // Notify the main thread that the generator is ready
   postMessage({ type: 'ready' });
 }
@@ -17,8 +18,10 @@ onmessage = async function(event) {
     if (!generator) {
       await initializeGenerator();
     }
-    const output = await generator(messages, options);
-    const result = output[0].generated_text.at(-1).content;
+    //const output = await generator(messages, options);
+    //const result = output[0].generated_text.at(-1).content;
+    const output = await generator(messages[1].content);
+    const result = output[0].translation_text;
     postMessage({ type: 'result', result });
   } catch (err) {
     postMessage({ type: 'error', error: err.message });
